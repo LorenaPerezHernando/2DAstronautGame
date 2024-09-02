ecb3cdeec5d4dab06f8282b0be9dcf2acba7b1ec
Lorena P <lorena.1997.lore@gmail.com>
2024-09-02 12:12:23 +0200

    Animaciones Completadas
    Dash, Down , tp son las que he incluido

--- Assets/Scripts/Animations/RobotAnimatorController.cs
+++ Assets/Scripts/Animations/RobotAnimatorController.cs
@@ -21,16 +21,41 @@ public class RobotAnimatorController : MonoBehaviour
     void Update()
     {
 		//Set Animator properties state
-		if (Input.GetKeyDown(KeyCode.RightArrow))
+		if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
 			_animator.SetBool("Run", true);
 
-		if (Input.GetKeyUp(KeyCode.RightArrow))
+		if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
 			_animator.SetBool("Run", false);
 
-		if (Input.GetKeyDown(KeyCode.Mouse0))
+
+
+		if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+			_animator.SetBool("Down", true); 
+
+		if(Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+			_animator.SetBool("Down", false) ;
+
+
+
+		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+			_animator.SetBool("Dash", true);
+
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+			_animator.SetBool("Dash", false);
+
+
+
+		if (Input.GetKeyDown(KeyCode.E))
+			_animator.SetTrigger("tp");
+
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
 			_animator.SetTrigger("Attack");
 
-		if (Input.GetKeyDown(KeyCode.Space))
+
+
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
 			_animator.SetTrigger("Jump");
 	}
 	#endregion
