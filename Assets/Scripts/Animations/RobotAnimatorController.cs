7a5f06cc9216db7fa03a4e48b7a4acde5263e3df
Lorena P <lorena.1997.lore@gmail.com>
2024-09-02 13:55:57 +0200

    Extras Animaciiones hacia la izq

--- Assets/Scripts/Animations/RobotAnimatorController.cs
+++ Assets/Scripts/Animations/RobotAnimatorController.cs
@@ -2,44 +2,69 @@ using UnityEngine;
 using System;
 
 public class RobotAnimatorController : MonoBehaviour
-{    
-    #region Properties
+{
+	#region Properties
+	public SpriteRenderer punch;
 	#endregion
 
 	#region Fields
 	[SerializeField] private Animator _animator;
+	private SpriteRenderer _spriteRenderer;
+	bool _izquierda = false;
 	#endregion
 
 	#region Unity Callbacks
 	// Start is called before the first frame update
 	void Start()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+		//punch = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+		if (_izquierda) _spriteRenderer.flipX = true; 
+
 		//Set Animator properties state
 		if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+		{ 
 			_animator.SetBool("Run", true);
-
+		    _spriteRenderer.flipX = false;
+			_izquierda = false; 
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+		{ 
+            _animator.SetBool("Run", true);
+		    _spriteRenderer.flipX = true;
+			_izquierda = true;
+        }
+  
 		if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
 			_animator.SetBool("Run", false);
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+            _animator.SetBool("Run", false);
 
 
 
-		if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-			_animator.SetBool("Down", true); 
 
-		if(Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+		{
+
+			_animator.SetBool("Down", true);
+			Izquierda();
+		}	
+		if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
 			_animator.SetBool("Down", false) ;
 
 
 
 		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+		{
 			_animator.SetBool("Dash", true);
-
+			Izquierda();
+		}
 		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
 			_animator.SetBool("Dash", false);
 
@@ -50,8 +75,14 @@ public class RobotAnimatorController : MonoBehaviour
 
 
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _izquierda == false)
+		{
 			_animator.SetTrigger("Attack");
+		}
+		else if (Input.GetKeyDown(KeyCode.Mouse0) && _izquierda == true)
+		{
+			_animator.SetTrigger("AttackIzq"); 
+		}
 
 
 
@@ -64,5 +95,13 @@ public class RobotAnimatorController : MonoBehaviour
 	#endregion
 
 	#region Private Methods
+
+	void Izquierda()
+	{
+		if(_izquierda == false)
+			_spriteRenderer.flipX = false;
+
+		else _spriteRenderer.flipX = true;
+	}
 	#endregion
 }
