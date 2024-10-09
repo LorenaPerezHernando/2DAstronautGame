d2cb39dd22495b02029ae1178e5c5d1af79b860b
Lorena P <lorena.1997.lore@gmail.com>
2024-10-09 14:13:51 +0200

    Notas

--- Assets/NotasParaProfe.cs
+++ Assets/NotasParaProfe.cs
@@ -11,11 +11,12 @@ public class NotasParaProfe : MonoBehaviour
      * Particle System del Jetpack cuando sube
      * Sonidos de chocar contra las plataformas y de las monedas
      * Luces dinamicas y animaciones para las estrellas de decoracion y las que hay que coleccionar
+     * Luces desde el player como si llevara una linterna 
      * 
      * Un Panel de final del recorrido 
      * 
      
-     
+     ESPERO QUE DISFRUTEN MUCHOP JUGANDO =)
       
      */
 }
